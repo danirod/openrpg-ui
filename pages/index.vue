77f1d5fb1c9b438b2cae375f5d6fb3a46b94f928
Dani Rodríguez <dani@danirod.es>
2020-01-19 21:59:42 +0100

    feat: backport home view into home route
    This commit migrates the following Vue components from the OpenRPG
    repository:
    
    * desktop_menu/Hamburguer.vue
    * desktop_menu/RadialMenu.vue
    * layers/overlay.vue
    
    In order to migrate the view home.vue to be used for the home route (/).
    As of this commit, opening the front page of the project should render
    the radial menu used to switch among views.

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,33 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        openrpg-ui
-      </h1>
-      <h2 class="subtitle">
-        GUI for OpenRPG
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div class="col-12">
+      <hamburguer-menu
+        @click.native="hidden = !hidden"
+        :overlay="hidden"
+      ></hamburguer-menu>
+      <overlay :hidden="hidden"> </overlay>
+      <radial-menu :hidden="hidden"></radial-menu>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import HamburguerMenu from '~/components/desktop_menu/HamburguerMenu'
+import RadialMenu from '~/components/desktop_menu/RadialMenu'
+import Overlay from '~/components/layers/overlay'
 
 export default {
   components: {
-    Logo
+    HamburguerMenu,
+    Overlay,
+    RadialMenu
+  },
+  data() {
+    return {
+      hidden: true
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style scoped></style>
