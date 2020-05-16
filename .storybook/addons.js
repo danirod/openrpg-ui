7154e653a25618d9296876bf3d2201b578c61cd0
Dani Rodríguez <dani@danirod.es>
2020-05-16 14:21:48 +0200

    feat(dev): install @storybook/addon-actions
    addon-actions is a Storybook addon that allows the Storybook UI to
    present dispatched actions under certain conditions. For instance,
    addon-actions can be used to view in the web browser an event emitted by
    a component, if the event handler dispatches a Storybook action.
    
    This commit also adds a Storybook story that emits an action when a
    SuccessMessage is clicked, as a demo to showcase how to handle Vue
    events dispatching Storybook actions.

---
+++ .storybook/addons.js
@@ -0,0 +1,6 @@
+import '@storybook/addon-actions/register'
+
+import { setConsoleOptions } from '@storybook/addon-console'
+setConsoleOptions({
+  panelExclude: []
+})
--- components/alerts/SuccessMessage.stories.js
+++ components/alerts/SuccessMessage.stories.js
@@ -1,8 +1,19 @@
+import { action } from '@storybook/addon-actions'
 import SuccessMessage from './SuccessMessage.vue'
 
-export default { title: 'SuccessMessage' }
+export default {
+  title: 'SuccessMessage'
+}
 
 export const withMessage = () => ({
   components: { SuccessMessage },
   template: `<success-message :success="true" successMessage="Jugador creado" />`
+})
+
+export const closeable = () => ({
+  components: { SuccessMessage },
+  methods: {
+    handleClick: action('clicked')
+  },
+  template: `<success-message :success="true" successMessage="Jugador creado" @click="handleClick" />`
 })
